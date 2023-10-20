825190ef3af1d54a2b4ccc3404107033293e60df
penguin011 <urayamaeizo@gmail.com>
2023-10-21 02:30:00 +0900

    Update App.tsx
    Co-Authored-By: anochkasky <134123427+anochkasky@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React, {lazy} from 'react';
-import logo from './logo.svg';
 import './App.css';
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <DefaultLayout/>
     </div>
   );
 }
