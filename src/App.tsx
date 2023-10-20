0f18345a209d14c14f8f647c2c7960e9472342fe
penguin011 <urayamaeizo@gmail.com>
2023-10-21 03:12:54 +0900

    created Loader
    Co-Authored-By: anochkasky <134123427+anochkasky@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React, { lazy } from 'react';
+import React, { lazy, useEffect, useState } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
+import Loader from './common/Loader';
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
 function App() {
-  return (
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <Routes>
       <Route path="/auth/signin" element={<SignIn/>}/>
       <Route path="/auth/signup" element={<SignUp/>}/>
