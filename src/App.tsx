cdef6675c00739724f3015a13af345117d9491c6
penguin011 <urayamaeizo@gmail.com>
2023-10-21 03:25:21 +0900

    update
    Co-Authored-By: anochkasky <134123427+anochkasky@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
+<<<<<<< Updated upstream
 const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
 function App() {
   return (
@@ -11,6 +12,34 @@ function App() {
       <Route path="/auth/signup" element={<SignUp/>}/>
       <Route element={<DefaultLayout />}/>
     </Routes>
+=======
+import Loader from './common/Loader';
+import { Toaster } from 'react-hot-toast';
+
+const DefaultLayout = lazy(() => import('./layout/DefaultLayout'));
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        containerClassName="overflow-auto"
+      />
+      <Routes>
+        <Route path="/auth/signin" element={<SignIn />} />
+        <Route path="/auth/signup" element={<SignUp />} />
+        <Route element={<DefaultLayout />} />
+      </Routes>
+    </>
+>>>>>>> Stashed changes
   );
 }
 
